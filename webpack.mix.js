c664736827e8d82ab9eb50558226ce6bcd40c77a
Zidan Mehedi <55471629+zidanMehedi@users.noreply.github.com>
2020-10-17 01:31:33 +0600

    npm installed and view pages included

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,30 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+	.js('resources/js/bootstrap.min.js', 'public/js')
+	.js('resources/js/jquery.nice-select.min.js', 'public/js')
+	.js('resources/js/jquery.slicknav.js', 'public/js')
+	.js('resources/js/jquery-3.3.1.min.js', 'public/js')
+	.js('resources/js/main.js', 'public/js')
+	.js('resources/js/mixitup.min.js', 'public/js')
+	.js('resources/js/owl.carousel.min.js', 'public/js')
+	.sass('resources/sass/app.scss', 'public/css');
+	.sass('resources/sass/_base.scss', 'public/css');
+	.sass('resources/sass/_blog.scss', 'public/css');
+	.sass('resources/sass/_blog-details.scss', 'public/css');
+	.sass('resources/sass/_blog-sidebar.scss', 'public/css');
+	.sass('resources/sass/_breadcrumb.scss', 'public/css');
+	.sass('resources/sass/_checkout.scss', 'public/css');
+	.sass('resources/sass/_contact.scss', 'public/css');
+	.sass('resources/sass/_footer.scss', 'public/css');
+	.sass('resources/sass/_header.scss', 'public/css');
+	.sass('resources/sass/_hero.scss', 'public/css');
+	.sass('resources/sass/_home-page.scss', 'public/css');
+	.sass('resources/sass/_mixins.scss', 'public/css');
+	.sass('resources/sass/_responsive.scss', 'public/css');
+	.sass('resources/sass/_shop-details.scss', 'public/css');
+	.sass('resources/sass/_shop-grid.scss', 'public/css');
+	.sass('resources/sass/_shopping-cart.scss', 'public/css');
+	.sass('resources/sass/_sidebar.scss', 'public/css');
+	.sass('resources/sass/_variable.scss', 'public/css');
+	.sass('resources/sass/style.scss', 'public/css');
